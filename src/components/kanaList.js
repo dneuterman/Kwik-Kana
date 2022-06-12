ea87b70efd4aba903a2446c03f52f39e5d24b54a
Donald Neuterman <dneuterman@gmail.com>
2022-06-12 01:51:28 -0400

    created separate file for list of kana

---
+++ src/components/kanaList.js
@@ -0,0 +1,377 @@
+const hiraganaArray = [
+  {
+    consonant: '-',
+    characters: [
+      {
+        character: 'あ',
+        romaji: 'a',
+      },
+      {
+        character: 'い',
+        romaji: 'i',
+      },
+      {
+        character: 'う',
+        romaji: 'u',
+      },
+      {
+        character: 'え',
+        romaji: 'e',
+      },
+      {
+        character: 'お',
+        romaji: 'o',
+      },
+    ]
+  },
+  {
+    consonant: 'k',
+    characters: [
+      {
+        character: 'か',
+        romaji: 'ka',
+      },
+      {
+        character: 'き',
+        romaji: 'ki',
+      },
+      {
+        character: 'く',
+        romaji: 'ku',
+      },
+      {
+        character: 'け',
+        romaji: 'ke',
+      },
+      {
+        character: 'こ',
+        romaji: 'ko',
+      },
+    ]
+  },
+  {
+    consonant: 's',
+    characters: [
+      {
+        character: 'さ',
+        romaji: 'sa',
+      },
+      {
+        character: 'し',
+        romaji: 'shi',
+      },
+      {
+        character: 'す',
+        romaji: 'su',
+      },
+      {
+        character: 'せ',
+        romaji: 'se',
+      },
+      {
+        character: 'そ',
+        romaji: 'so',
+      },
+    ]
+  },
+  {
+    consonant: 't',
+    characters: [
+      {
+        character: 'さ',
+        romaji: 'ta',
+      },
+      {
+        character: 'し',
+        romaji: 'chi',
+      },
+      {
+        character: 'す',
+        romaji: 'tsu',
+      },
+      {
+        character: 'せ',
+        romaji: 'te',
+      },
+      {
+        character: 'そ',
+        romaji: 'to',
+      },
+    ]
+  },
+  {
+    consonant: 'n',
+    characters: [
+      {
+        character: 'な',
+        romaji: 'na',
+      },
+      {
+        character: 'に',
+        romaji: 'ni',
+      },
+      {
+        character: 'ぬ',
+        romaji: 'nu',
+      },
+      {
+        character: 'ね',
+        romaji: 'ne',
+      },
+      {
+        character: 'の',
+        romaji: 'no',
+      },
+    ]
+  },
+  {
+    consonant: 'h',
+    characters: [
+      {
+        character: 'は',
+        romaji: 'ha',
+      },
+      {
+        character: 'ひ',
+        romaji: 'hi',
+      },
+      {
+        character: 'ふ',
+        romaji: 'fu',
+      },
+      {
+        character: 'へ',
+        romaji: 'he',
+      },
+      {
+        character: 'ほ',
+        romaji: 'ho',
+      },
+    ]
+  },
+  {
+    consonant: 'm',
+    characters: [
+      {
+        character: 'ま',
+        romaji: 'ma',
+      },
+      {
+        character: 'み',
+        romaji: 'mi',
+      },
+      {
+        character: 'む',
+        romaji: 'mu',
+      },
+      {
+        character: 'め',
+        romaji: 'me',
+      },
+      {
+        character: 'も',
+        romaji: 'mo',
+      },
+    ]
+  },
+  {
+    consonant: 'y',
+    characters: [
+      {
+        character: 'や',
+        romaji: 'ya',
+      },
+      null,
+      {
+        character: 'ゆ',
+        romaji: 'yu',
+      },
+      null,
+      {
+        character: 'よ',
+        romaji: 'yo',
+      },
+    ]
+  },
+  {
+    consonant: 'r',
+    characters: [
+      {
+        character: 'ら',
+        romaji: 'ra',
+      },
+      {
+        character: 'り',
+        romaji: 'ri',
+      },
+      {
+        character: 'る',
+        romaji: 'ru',
+      },
+      {
+        character: 'れ',
+        romaji: 're',
+      },
+      {
+        character: 'ろ',
+        romaji: 'ro',
+      },
+    ]
+  },
+  {
+    consonant: 'w',
+    characters: [
+      {
+        character: 'わ',
+        romaji: 'wa',
+      },
+      null,
+      null,
+      null,
+      {
+        character: 'を',
+        romaji: 'wo',
+      },
+    ]
+  },
+  {
+    consonant: '(n)',
+    characters: [
+      {
+        character: 'ん',
+        romaji: 'n',
+      },
+      null,
+      null,
+      null,
+      null,
+    ]
+  },
+  {
+    consonant: 'g',
+    characters: [
+      {
+        character: 'が',
+        romaji: 'ga',
+      },
+      {
+        character: 'ぎ',
+        romaji: 'gi',
+      },
+      {
+        character: 'ぐ',
+        romaji: 'gu',
+      },
+      {
+        character: 'げ',
+        romaji: 'ge',
+      },
+      {
+        character: 'ご',
+        romaji: 'go',
+      },
+    ]
+  },
+  {
+    consonant: 'z',
+    characters: [
+      {
+        character: 'ざ',
+        romaji: 'za',
+      },
+      {
+        character: 'じ',
+        romaji: 'ji',
+      },
+      {
+        character: 'ず',
+        romaji: 'zu',
+      },
+      {
+        character: 'ぜ',
+        romaji: 'ze',
+      },
+      {
+        character: 'ぞ',
+        romaji: 'zo',
+      },
+    ]
+  },
+  {
+    consonant: 'd',
+    characters: [
+      {
+        character: 'だ',
+        romaji: 'da',
+      },
+      {
+        character: 'ぢ',
+        romaji: 'ji',
+      },
+      {
+        character: 'づ',
+        romaji: 'zu',
+      },
+      {
+        character: 'で',
+        romaji: 'de',
+      },
+      {
+        character: 'ど',
+        romaji: 'do',
+      },
+    ]
+  },
+  {
+    consonant: 'b',
+    characters: [
+      {
+        character: 'ば',
+        romaji: 'ba',
+      },
+      {
+        character: 'び',
+        romaji: 'bi',
+      },
+      {
+        character: 'ぶ',
+        romaji: 'bu',
+      },
+      {
+        character: 'べ',
+        romaji: 'be',
+      },
+      {
+        character: 'ぼ',
+        romaji: 'bo',
+      },
+    ]
+  },
+  {
+    consonant: 'p',
+    characters: [
+      {
+        character: 'ぱ',
+        romaji: 'pa',
+      },
+      {
+        character: 'ぴ',
+        romaji: 'pi',
+      },
+      {
+        character: 'ぷ',
+        romaji: 'pu',
+      },
+      {
+        character: 'ぺ',
+        romaji: 'pe',
+      },
+      {
+        character: 'ぽ',
+        romaji: 'po',
+      },
+    ]
+  },
+]
+
+export {hiraganaArray};
